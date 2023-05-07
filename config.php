d715ae80c44a30bda4bfe271568f7342a6d3abd4
Huynh Truong Hien Vinh <128971146+hthvinh74@users.noreply.github.com>
2023-05-07 10:44:18 +0700

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$db_host = "us-cdbr-east-06.cleardb.net";
-$db_user = "b8f8f2f25c66e1";
-$db_pass = "dce09ba4";
-$db_name = "heroku_aea989f3edb4771";
-
-$connect = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("database connection error");
-?>
