7167d1dd608d8b23d789d7e4ca5b95e0a470f917
Huynh Truong Hien Vinh <128971146+hthvinh74@users.noreply.github.com>
2023-05-07 10:35:42 +0700

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,8 +1,8 @@
 <?php
-    define('HOST', '127.0.0.1');
-    define('USER', 'root');
-    define('PASS','');
-    define('DB_NAME', 'musclewikison');
+    define('HOST', 'us-cdbr-east-06.cleardb.net');
+    define('USER', 'b8f8f2f25c66e1');
+    define('PASS','dce09ba4');
+    define('DB_NAME', 'heroku_aea989f3edb4771');
 
     function create_connection(){
         $connect = new mysqli(HOST,USER,PASS,DB_NAME);
